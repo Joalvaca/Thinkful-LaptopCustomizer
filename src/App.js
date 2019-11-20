4af314c864a5d7a55bbb1750f339f398edeee1dd
joel alva <panthersjoe@hotmail.com>
2019-11-20 17:20:55 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,100 +1,69 @@
 import React, { Component } from 'react';
-import Header from './Components/Header'
+import Customize from './Components/Customize';
+import Cart from './Components/Cart';
+
+import slugify from 'slugify';
 import './App.css';
-import ListOfParts from './Components/Total';
-import Total from './Components/Total'
+
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selected: {
-        Processor: {
-            name: '17th Generation Intel Core HB (7 Core with donut spare)',
-            cost: 700
-          },
-        "Operating System": {
-            name: 'Ubuntu Linux 16.04',
-            cost: 200
-          },
-        "Video Card":{
-            name: 'Toyota Corolla 1.5v',
-            cost: 1150.98
-          },
-        Display: {
-            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-            cost: 1500
-          }
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
       },
-      features: {
-        Processor: [
-          {
-            name: '17th Generation Intel Core HB (7 Core with donut spare)',
-            cost: 700
-          },
-          {
-            name: 'Professor X AMD Fire Breather with sidewinder technology',
-            cost: 1200
-          }
-        ],
-        "Operating System": [
-          {
-            name: 'Ubuntu Linux 16.04',
-            cost: 200
-          },
-          {
-            name: 'Bodhi Linux',
-            cost: 300
-          }
-        ],
-        "Video Card": [
-          {
-            name: 'Toyota Corolla 1.5v',
-            cost: 1150.98
-          },
-          {
-            name: 'Mind mild breeze 2000',
-            cost: 1345
-          }
-        ],
-        Display: [
-          {
-            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-            cost: 1500
-          },
-          {
-            name: '15.3" HGTV (3840 x 2160) Home makeover edition',
-            cost: 1400
-          },
-        ]
-      }  
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
     }
-  }
+  };
 
+  
 
-  updateFeature(feature, newValue) {
+  updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
       selected
     });
-  }
+  };
 
   render() {
     return (
-      <div className="App">
-        <Header />   
-        <main>
-        
-          
-        <ListOfParts updateFeature={((feature, newValue) =>  this.updateFeature(feature, newValue))} selected={this.state.selected} features={this.state.features}/>
-
-         <Total selected={this.state.selected}/>
-        </main>
-        
-      </div>
+    <div>
+      <header>
+          <h1>ELF Computing | Laptops</h1>
+      </header>
+      <main>
+        <Customize 
+                    selected={this.state.selected} 
+                    currency={USCurrencyFormat}  
+                    features={this.props.features} 
+                    select={this.updateFeature}
+        />
+        <Cart
+                    selected={this.state.selected}
+                    currency={USCurrencyFormat}
+        />
+      </main>
+    </div>
     );
   }
 }
 
-export default App;  
+export default App;
